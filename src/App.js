3680dff072e282b259d6d254799051b1d37605cd
sophiasundar <sophiagifta17@gmail.com>
2023-06-18 13:04:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,123 +1,65 @@
 import './App.css';
-import Cards from './Components/Cards';
+import Cards from './Components/Cards'
 
 
-function App() {
-  var cardDetails=[
+
+function App(){
+  const Cards = [
     {
-      planName:"FREE",
-      price: 0,
-      features :[
-        {
-          name: "Single User",isEnabled:true
-        },
-        {
-          name: "5GB Storage",isEnabled:true
-        },
-        {
-          name: "Unlimited Public Projects",isEnabled:true
-        },
-        {
-          name: "Community Access",isEnabled:true
-        },
-        {
-          name: "Unlimited Private Projects",isEnabled:false
-        },
-        {
-          name: "Dedicated Phone Support",isEnabled:false
-        },
-        {
-          name: "Free Subdomain",isEnabled:false
-        },
-        {
-          name: "Monthly Status Reports",isEnabled:false
-        },
-
-      ],
-    },
-    {
-      planName:"PLUS",
-      price: 9,
-      features :[
-        {
-          name: "5 User",isEnabled:true,isBold: true
-        },
-        {
-          name: "50GB Storage",isEnabled:true
-        },
-        {
-          name: "Unlimited Public Projects",isEnabled:true
-        },
-        {
-          name: "Community Access",isEnabled:true
-        },
-        {
-          name: "Unlimited Private Projects",isEnabled:true
-        },
-        {
-          name: "Dedicated Phone Support",isEnabled:true
-        },
-        {
-          name: "Free Subdomain",isEnabled:true
-        },
-        {
-          name: "Monthly Status Reports",isEnabled:false
-        },
-
-      ],
-    },
-    {
-      planName:"PRO",
-      price: 49,
-      features :[
-        {
-          name: "Unlimited User",isEnabled:true,isBold: true
-        },
-        {
-          name: "150GB Storage",isEnabled:true
-        },
-        {
-          name: "Unlimited Public Projects",isEnabled:true
-        },
-        {
-          name: "Community Access",isEnabled:true
-        },
-        {
-          name: "Unlimited Private Projects",isEnabled:true
-        },
-        {
-          name: "Dedicated Phone Support",isEnabled:true
-        },
-        {
-          name: "Unlimited Free Subdomain",isEnabled:true,isunlimited: true
-        },
-        {
-          name: "Monthly Status Reports",isEnabled:true
-        },
-
-      ],
-    },
-  ];
-  return (
-
-    <div className="App">
-       <Cards></Cards>
-      <section class="pricing py-5">
-      <div class="container">
-      <div class="row">
-       
-        {
-          cardDetails.map((data,index)=>{
-            return <Cards key={`C${index}`} data={data}></Cards>
-          })
-        }
-
-
-      </div>
-      </div>
-      </section>
-    </div>
-  );
+  planName:"Free",
+  price: 0,
+  features: [
+    {name:"single-user",enabled:true },
+    {name:"5GB Storage",enabled:true },
+    {name:"Unlimited Public Projects",enabled:true },
+    {name:"Community Access",enabled:true },
+    {name:"Unlimited Private Projects",enabled:false },
+    {name:"Dedicated Phone Support",enabled:false },
+    {name:"Free Subdomain",enabled:false },
+    {name:"Monthly Status Reports",enabled:false },
+  ],
+},
+{ 
+planName:"Plus",
+price: 9,
+features: [
+  {name:"single-user",enabled:true },
+  {name:"5GB Storage",enabled:true },
+  {name:"Unlimited Public Projects",enabled:true },
+  {name:"Community Access",enabled:true },
+  {name:"Unlimited Private Projects",enabled:true },
+  {name:"Dedicated Phone Support",enabled:true },
+  {name:"Free Subdomain",enabled:true },
+  {name:"Monthly Status Reports",enabled:false },
+],
+},
+{
+  planName:"Pro",
+  price: 49,
+  features: [
+    {name:"single-user",enabled:true },
+    {name:"5GB Storage",enabled:true },
+    {name:"Unlimited Public Projects",enabled:true },
+    {name:"Community Access",enabled:true },
+    {name:"Unlimited Private Projects",enabled:true },
+    {name:"Dedicated Phone Support",enabled:true },
+    {name:"Free Subdomain",enabled:true },
+    {name:"Monthly Status Reports",enabled:true },
+  ],
+},
+  ];  
+return( 
+  <section className="pricing py-5">
+ <div className="container">
+   <div className="row">
+     {
+       Cards.map((card) =>{
+          
+          return <Cards key={App} planName={card.planName} name={card.name} price={card.price} features= {card.features}/>
+       })
+     }
+   </div>
+ </div></section>
+);
 }
-
 export default App;
